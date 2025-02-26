6407adf9a266a009dbdb68952aa2d0da825f10e2
psdox <duongnguyenthai83@gmail.com>
2025-02-26 19:16:52 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('have a gud night and hope you like this').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
